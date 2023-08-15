6ad1582e93938568e8a2320cdd46fcb7f523f011
consti10 <geierconstantinabc@gmail.com>
2023-08-15 17:28:09 +0200

    openhd debug, fix tx power to max

--- hal/hal_com_phycfg.c
+++ hal/hal_com_phycfg.c
@@ -2238,8 +2238,8 @@ phy_set_tx_power_index_by_rate_section(
 		// Consti10
 		// Really really dirty - 52 is a sane but increadibly high value. lets see what happens when
 		// we "do something similar" as rtl8812au ;)
-	    RTW_INFO("OpenHD: applying fixed power index 50, original: %d\n",(int)powerIndex);
-		powerIndex=50;
+	    //RTW_WARN("OpenHD: applying fixed power index 62, original: %d\n",(int)powerIndex);
+		//powerIndex=62;
 		PHY_SetTxPowerIndex(pAdapter, powerIndex, RFPath, rates_by_sections[rs].rates[i]);
 	}
 
--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -4624,6 +4624,14 @@ static int cfg80211_rtw_set_txpower(struct wiphy *wiphy,
 	default:
 		RTW_WARN(FUNC_WIPHY_FMT" unknown type:%d\n", FUNC_WIPHY_ARG(wiphy), type);
 	}
+    // 30dBm == 1W
+    // Fixed
+    // Hard-coded - if the chip cannot do 1W (I don't know any so far that can do that) the driver seems to just
+    // set whatever highest is possible
+    wiphy_data->txpwr_total_lmt_mbm = UNSPECIFIED_MBM;
+    wiphy_data->txpwr_total_target_mbm= 30*1000;
+    RTW_WARN(FUNC_WIPHY_FMT" OpenHD cf80211 tx power %s txpwr_total_lmt_mbm:%d txpwr_total_target_mbm%d \n", FUNC_WIPHY_ARG(wiphy)
+		, nl80211_tx_power_setting_str(type), wiphy_data->txpwr_total_lmt_mbm,wiphy_data->txpwr_total_target_mbm);
 
 	if (ret == 0)
 		rtw_run_in_thread_cmd_wait(adapter, ((void *)(rtw_hal_update_txpwr_level)), adapter, 2000);
