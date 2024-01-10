0e3ce97ee34e99bf473b056ca6cb9ae8a0f2d146
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-01-10 01:35:06 +0100

    Update before-install.sh

--- before-install.sh
+++ before-install.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sudo rm -Rf /lib/modules/6.3.13-060313-generic/kernel/drivers/net/wireless/8852bu_ohd.ko
+
+if [ -e "/lib/modules/6.3.13-060313-generic/kernel/drivers/net/wireless/rtl8852bu.ko" ]; then
+    sudo rm -Rf /lib/modules/6.3.13-060313-generic/kernel/drivers/net/wireless/rtl8852bu.ko
+fi
+if [ -e "/lib/modules/6.3.13-060313-generic/kernel/drivers/net/wireless/rtl8852bu_ohd.ko" ]; then
+    sudo rm -Rf /lib/modules/6.3.13-060313-generic/kernel/drivers/net/wireless/rtl8852bu_ohd.ko
+fi
